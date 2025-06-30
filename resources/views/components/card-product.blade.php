2934049055e0b484024d42d765a66b24ade8df3e
aridwann <aridwansaja84@gmail.com>
2025-06-30 12:15:15 +0700

    Update readme

--- resources/views/components/card-product.blade.php
+++ resources/views/components/card-product.blade.php
@@ -1,21 +1,19 @@
 @props(['product'])
 
-<div 
-  class="flex flex-col gap-3 pb-3 hover:scale-105 transition cursor-pointer" 
-  onclick="location.href = 'products/{{ $product['id'] }}'">
-    <div
-      class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
-      style="background-image: url({{ $product['img'] }});">
+<div class="flex flex-col gap-3 pb-3 hover:scale-105 transition cursor-pointer"
+    onclick="location.href = 'products/{{ $product['id'] }}'">
+    <div class="w-full bg-center bg-no-repeat aspect-square bg-cover rounded-xl"
+        style="background-image: url({{ $product['img'] }});">
     </div>
     <div>
-      <p class="text-[#191410] text-base font-medium leading-normal">
-        {{ $product['name'] }}
-      </p>
-      <p class="text-[#8c735a] text-sm font-normal leading-normal">
-        Terjual: @convert($product['sold'])
-      </p>
-      <p class="text-[#f38920] text-base font-bold leading-normal my-2">
-        Rp. @convert($product['price'])
-      </p>
+        <p class="text-[#191410] text-base font-medium leading-normal">
+            {{ $product['name'] }}
+        </p>
+        <p class="text-[#8c735a] text-sm font-normal leading-normal">
+            Terjual: @convert($product['sold'])
+        </p>
+        <p class="text-[#f38920] text-base font-bold leading-normal my-2">
+            Rp. @convert($product['price'])
+        </p>
     </div>
 </div>
