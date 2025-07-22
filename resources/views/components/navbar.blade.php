d458573b058df21f3749e4ac695d42905fc055ad
aridwann <aridwansaja84@gmail.com>
2025-07-22 15:10:08 +0700

    show cart even its in dashboard

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -35,18 +35,14 @@
     @auth
         <form action="/logout" method="POST" class="flex gap-4 items-center relative" x-data="{ show: false }">
             @csrf
-            {{-- Jika berada di dashboard, jangan tampilkan cart --}}
-            @if (!request()->is('dashboard'))
-                <a href="/cart"
-                    class="flex gap-1 h-7 min-w-[54px] max-w-[480px] font-medium cursor-pointer items-center justify-center overflow-hidden px-2 text-white text-sm leading-normal tracking-[0.015em]  {{ request()->is('cart') ? 'bg-[#344a29] rounded-sm' : '' }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="14px"
-                        fill=white>
-                        <path
-                            d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
-                    </svg>
-                    Keranjang
-                </a>
-            @endif
+            <a href="/cart"
+                class="flex gap-1 h-7 min-w-[54px] max-w-[480px] font-medium cursor-pointer items-center justify-center overflow-hidden px-2 text-white text-sm leading-normal tracking-[0.015em]  {{ request()->is('cart') ? 'bg-[#344a29] rounded-sm' : '' }}">
+                <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="14px" fill=white>
+                    <path
+                        d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+                </svg>
+                Keranjang
+            </a>
             <div class="select-none bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10 cursor-pointer hover:scale-95"
                 @click="show = !show"
                 style="background-image: url('{{ asset(Auth::user()->avatar ?? 'img/default-avatar.png') }}');">
