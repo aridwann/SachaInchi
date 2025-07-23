b749c7696d50dcd1e9195971138f1e15b8a5ca44
aridwann <aridwansaja84@gmail.com>
2025-07-23 12:27:21 +0700

    responsive detail-product

--- resources/views/detail-product.blade.php
+++ resources/views/detail-product.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="px-40 flex flex-1 justify-center py-5">
+    <div class="px-8 lg:px-40 flex flex-1 justify-center py-5">
         <div class="layout-content-container flex flex-col max-w-[960px] flex-1">
             @if (session('success'))
                 <div x-data="{ show: true }" x-transition.duration.500ms x-init="setTimeout(() => show = false, 2500)" x-show="show"
@@ -14,7 +14,7 @@
                 <span class="text-white text-base font-medium leading-normal">{{ $product['name'] }}</span>
             </div>
 
-            <div class="flex">
+            <div class="flex flex-wrap">
                 <div class="w-100 m-4 gap-1 overflow-hidden bg-white aspect-[9/10] rounded-xl flex">
                     <div class="w-full bg-center bg-no-repeat aspect-auto bg-cover rounded-none flex-1"
                         style="background-image: url({{ asset($product->img) }});">
@@ -40,7 +40,7 @@
 
                         <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4 m-5">
                             <dt class="font-medium text-[#A3C299]">Stok</dt>
-                            <dd class="text-[#A3C299] sm:col-span-2">
+                            <dd class="text-[#A3C299]">
                                 <div class="flex items-center rounded  w-30">
                                     {{ $product->stock }}
                                 </div>
@@ -49,7 +49,7 @@
                         @if ($product->stock == 'Tersedia')
                             <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4 m-5">
                                 <dt class="font-medium text-[#A3C299]">Jumlah</dt>
-                                <dd class="text-[#A3C299] sm:col-span-2">
+                                <dd class="text-[#A3C299]">
                                     <div class="flex items-center rounded border border-[#A3C299] w-30">
                                         <button type="button" id="kurangBtn"
                                             class="size-10 leading-10 text-[#A3C299] transition hover:opacity-75 ">&minus;</button>
